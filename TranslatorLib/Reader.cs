75ae6bb2918934802cc15fdec97800ba2ba924e3
Hedrig <medaldm@mail.ru>
2016-09-28 18:52:42 +0300

    Лабораторная работа №1

---
+++ TranslatorLib/Reader.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TranslatorLib
+{
+    static class Reader
+    {
+        public static int CurrentSymbol
+        {
+            get
+            {
+                return currentSymbol;
+            }
+        }
+
+        public static int RowIndex
+        {
+            get
+            {
+                return rowIndex;
+            }
+        }
+
+        public static int ColumnIndex
+        {
+            get
+            {
+                return columnIndex;
+            }
+        }
+
+        static int currentSymbol;
+        static int rowIndex;
+        static int columnIndex;
+        static StreamReader streamReader;
+
+        public static void Initialize(string filename)
+        {
+            streamReader.Close();
+            streamReader = new StreamReader(filename);
+            rowIndex = 1;
+            columnIndex = 1;
+        }
+        public static void ReadNextSymbol()
+        {
+            currentSymbol = streamReader.Read();
+            switch (currentSymbol)
+            {
+                case ('\0'):
+                    {
+                        break;
+                    }
+                case ('\n'):
+                    {
+                        rowIndex++;
+                        columnIndex = 1;
+                        break;
+                    }
+                case ('\t'):
+                case ('\r'):
+                    {
+                        ReadNextSymbol();
+                        break;
+                    }
+                default:
+                    {
+                        columnIndex++; break;
+                    }
+            }
+        }
+        public static void Close()
+        {
+            streamReader.Close();
+        }
+    }
+}
