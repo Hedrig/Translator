cfd1765f204f031e7603c2363b779b2617319769
Hedrig <medaldm@mail.ru>
2016-10-11 11:06:25 +0300

    Добавил класс для синтаксического анализатора

---
+++ TranslatorLib/SyntaxAnalyzer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranslatorLib
+{
+    static class SyntaxAnalyzer
+    {
+        static void CheckLexem(Lexems expectedLexem)
+        {
+            if(LexicalAnalyzer.CurrentLexem != expectedLexem)
+                throw new UnexpectedLexemError();
+            
+        }
+    }
+}
---
+++ TranslatorLib/UnexpectedLexemError.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace TranslatorLib
+{
+    [Serializable]
+    internal class UnexpectedLexemError : Exception
+    {
+        public UnexpectedLexemError()
+        {
+        }
+
+        public UnexpectedLexemError(string message) : base(message)
+        {
+        }
+
+        public UnexpectedLexemError(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected UnexpectedLexemError(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
