da7e66a3215f8927b780d8621c6860c0bb1d975c
Hedrig <medaldm@mail.ru>
2016-10-14 14:50:59 +0300

    Переименован класс исключения UnexpectedLexemError

--- TranslatorLib/SyntaxAnalyzer.cs
+++ TranslatorLib/SyntaxAnalyzer.cs
@@ -19,7 +19,7 @@ namespace TranslatorLib
         static void CheckLexem(Lexems expectedLexem)
         {
             if (LexicalAnalyzer.CurrentLexem != expectedLexem)
-                throw new UnexpectedLexemError("Ожидалась лексема " + expectedLexem + ", получена лексема " +
+                throw new UnexpectedLexemException("Ожидалась лексема " + expectedLexem + ", получена лексема " +
                     LexicalAnalyzer.CurrentLexem);
             else
                 LexicalAnalyzer.DecodeLexem();
